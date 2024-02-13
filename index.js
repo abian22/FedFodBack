6a49aee3f8a8ee35e0aa462066809987f7707924
Abián <70582564+abian22@users.noreply.github.com>
2024-02-13 11:11:45 +0000

    origin of app.use changed

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function startExpress() {
   app.use(passport.initialize());
   app.use(
     cors({
-      origin: "http://localhost:4000",
-      // origin: "https://feedfoodback.onrender.com",
+      // origin: "http://localhost:4000",
+       origin: "https://feedfoodback.onrender.com",
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
   )
