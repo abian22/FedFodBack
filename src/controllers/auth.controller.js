c6516558ce863dfff06cde049ab33e55a63a93cb
Abián <70582564+abian22@users.noreply.github.com>
2024-02-28 12:38:52 +0000

    check if email is already in use in signup controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,6 +5,12 @@ require("dotenv").config();
 
 const signUp = async (req, res) => {
   try {
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already in use" });
+    }
+
     req.body.password = bcrypt.hashSync(req.body.password, 10);
     const user = await User.create(req.body);
     const token = jwt.sign({ email: user.email }, process.env.SECRET, {
