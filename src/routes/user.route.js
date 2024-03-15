cad8945f62310812b1c387fa9398367651f4ec98
Abián <70582564+abian22@users.noreply.github.com>
2024-03-15 10:22:35 +0000

    search user changed from get to pos

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -18,11 +18,11 @@ const { login, signUp } = require("../controllers/auth.controller")
 
 router.get("/", checkAuth, checkAdmin, getAllUsers) //checked
 router.get("/me", checkAuth, getMe) //checked
-router.get("/search", checkAuth, searchUsers) //checked
 router.get("/:identifier", checkAuth, getOneUser) //checked
 router.post("/", checkAuth, checkAdmin, createUser) //checked
 router.post("/signUp", signUp) //checked
 router.post("/login", login) //checked
+router.post("/search", checkAuth, searchUsers) //checked
 router.put("/me", checkAuth, updateMe) //checked
 router.put("/:id", checkAuth, checkAdmin, updateUser) //checked
 router.delete("/me", checkAuth, deleteMe) //checked
