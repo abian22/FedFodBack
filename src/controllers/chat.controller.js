ceaacbe9839045d2e2c8331f856fe3a0d3762742
Abián <70582564+abian22@users.noreply.github.com>
2024-04-04 10:54:11 +0100

    getmessages modified

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -2,43 +2,49 @@ const Chat = require("../models/chat.model");
 
 const getMessages = async (req, res) => {
   try {
-    const userId = req.params.userId; 
+    const currentUser = res.locals.user;
+    const otherUserId = req.params.userId;
 
-    const receivedChats = await Chat.find({ receiver: userId });
-    const sentChats = await Chat.find({ sender: userId });
+    const receivedChat = await Chat.find({
+      receiver: otherUserId,
+      sender: currentUser._id,
+    });
+    const sentChat = await Chat.find({
+      sender: currentUser._id,
+      receiver: otherUserId,
+    });
 
-    // Concatenar los chats y ordenarlos por fecha de creación de manera descendente
-    const allChats = receivedChats.concat(sentChats).sort((a, b) => a.createdAt - b.createdAt);
+    const allChats = receivedChat
+      .concat(sentChat)
+      .sort((a, b) => a.createdAt - b.createdAt);
 
     res.status(200).json(allChats);
   } catch (error) {
-    // En caso de error, pasa el control al middleware de manejo de errores
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener los mensajes" });
   }
-}
+};
+const sendMessage = async (req, res) => {
+  try {
+    const { receiver, message } = req.body;
 
-  const sendMessage = async (req, res) => {
-    try {
-      const { receiver, message } = req.body;
-      
-      const newChat = new Chat({
-        sender: res.locals.user.id,
-        receiver: receiver,
-        message: message,
-      });
-  
-      await newChat.save();
-      console.log("Mensaje guardado en la base de datos");
-  
-      res.json(newChat);
-    } catch (error) {
-      console.error("Error al guardar el mensaje:", error);
-      res.status(500).json({ error: "Error al guardar el mensaje" });
-    }
-  };
-  
-  
+    const newChat = new Chat({
+      sender: res.locals.user.id,
+      receiver: receiver,
+      message: message,
+    });
+
+    await newChat.save();
+    console.log("Mensaje guardado en la base de datos");
+
+    res.json(newChat);
+  } catch (error) {
+    console.error("Error al guardar el mensaje:", error);
+    res.status(500).json({ error: "Error al guardar el mensaje" });
+  }
+};
 
 module.exports = {
   sendMessage,
-  getMessages
+  getMessages,
 };
