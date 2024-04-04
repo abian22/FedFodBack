2989b76dfff1a9a5b616bd41a3dbc1ac796a8b7c
Abián <70582564+abian22@users.noreply.github.com>
2024-04-04 12:00:25 +0100

    getmessaged modified

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -5,26 +5,32 @@ const getMessages = async (req, res) => {
     const currentUser = res.locals.user.id;
     const otherUserId = req.params.id;
 
+    // Obtener mensajes recibidos y enviados
     const receivedChat = await Chat.find({
       receiver: otherUserId,
-      sender: currentUser ,
+      sender: currentUser,
     });
     const sentChat = await Chat.find({
-      sender: currentUser,
-      receiver: otherUserId,
+      sender: otherUserId,
+      receiver: currentUser,
     });
-    console.log(otherUserId)
 
-    const allChats = receivedChat
-      .concat(sentChat)
-      .sort((a, b) => a.createdAt - b.createdAt);
-    console.log(allChats)
+    // Crear un conjunto (Set) para almacenar mensajes únicos
+    const allChatsSet = new Set([...receivedChat, ...sentChat]);
+
+    // Convertir el conjunto de nuevo en un array
+    const allChats = Array.from(allChatsSet);
+
+    console.log(allChats);
+
+    // Enviar la respuesta JSON con los mensajes
     res.status(200).json(allChats);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error al obtener los mensajes" });
   }
 };
+
 
 const sendMessage = async (req, res) => {
   try {
