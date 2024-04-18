433b34ed8ae6d7e1641f89061b9eb24d7974a61f
Abián <70582564+abian22@users.noreply.github.com>
2024-04-18 17:42:09 +0100

    google route deleted

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router()
 
 router.use("/user", require("./user.route"))
 router.use("/media", require("./media.route"))
-router.use("/google", require("./google.route"))
 router.use("/comment", require("./comment.route"))
 router.use("/notification", require("./notification.route"))
 router.use("/chat", require("./chat.route"))
